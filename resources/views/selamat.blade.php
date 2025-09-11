dd8b28ef94a9a343e378aa372e37d4d3618a343e
AchmadAlvin <achmadalvin886@gmail.com>
2025-09-11 22:19:54 +0700

    controller add

---
+++ resources/views/selamat.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang</title>
+
+    <style>
+        body{
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+    
+</head>
+<body>
+    <h1>Selamat Datang Twin</h1>
+    <img src="{{ asset('love.png') }}" alt="" width="400">
+    <p>it is what it is.</p>
+</body>
+</html>
